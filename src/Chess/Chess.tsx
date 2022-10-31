397bf1f2330f4c06584463a82cd8a633d128c61b
M <msanlop@github.com>
2022-10-31 22:37:58 +0100

    add some of the logic for calculating possible moves

--- src/Chess/Chess.tsx
+++ src/Chess/Chess.tsx
@@ -1,14 +1,38 @@
-
+export const BOARD_SIZE = 8;
 
 const starterPositionSerialized = 
-`br:bk:bb:bq:bk:bb:bk:br:\
+`br:bk:bb:bq:bK:bb:bk:br:\
 bp:bp:bp:bp:bp:bp:bp:bp:\
 ::::::::\
 ::::::::\
 ::::::::\
 ::::::::\
 wp:wp:wp:wp:wp:wp:wp:wp:\
-wr:wk:wb:wq:wk:wb:wk:wr`
+wr:wk:wb:wq:wK:wb:wk:wr`
+
+interface Piece {
+    piece: string;
+    color: string;
+}
+
+export interface Coordinate {
+    x : number;
+    y : number;
+}
+
+export interface Board {
+    board : (Piece | null)[];
+}
+
+interface GameState {
+    turn : string; // boolean?
+    board : (Piece | null)[];
+    // times ?: number; //timers??
+}
+
+const strides : Map<string, number[][]> = new Map([
+    ['k', [[-1, 2], [1, 2], [-1, -2], [1, -2]]],
+])
 
 
 const deserialize = (pos : string) => {
@@ -20,9 +44,38 @@ const deserialize = (pos : string) => {
 
 const starterPosition : ({piece: string, color: string} | null)[] = deserialize(starterPositionSerialized)
 
+export const getPiece = (coords : Coordinate, state : GameState) => {
+    const {x, y} = coords;
+    
+    return state.board[x + y*BOARD_SIZE];
+}
 
 const isGameFinished = () => {
     return false
+}
+
+export const getAllowedMoves = (coords : Coordinate, state : GameState) => {    
+    const piece = getPiece(coords, state)    
+    if(piece === null || piece.color !== state.turn){return [];}
+    
+    const moves : boolean[] = new Array(BOARD_SIZE ** 2).fill(false)
+    const {x, y} = coords;
+    const strideArray = strides.get(piece.piece)    
+    if(!strideArray){return []}
+
+    for (const stride of strideArray) {
+        const newCoord = {x : x + stride[0], y: y + stride[1]}
+        if(newCoord.x >= BOARD_SIZE || newCoord.x < 0 || newCoord.y >= BOARD_SIZE || newCoord.y < 0){
+            continue;
+        }
+
+        if(getPiece(newCoord, state) !== null){
+            //check if takes is possible
+            break;
+        }
+        moves[newCoord.x + BOARD_SIZE*newCoord.y] = true
+    }
+    return moves;
 }
 
 
