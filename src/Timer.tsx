0b45030e60c01259ffc97e3892b055a45a3c4282
Martin <msanlop@github.com>
2022-11-12 01:58:53 +0100

    fix : make const cause why not

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -5,7 +5,7 @@ const Timer = (props:any) => {
     const msToMin = (ms:number) => {
         const min = Math.floor((ms/ (1000 *60)));
         const sec = Math.floor((ms/1000) % 60);
-        let secString = sec < 10 ? '0' + String(sec) : String(sec)
+        const secString = sec < 10 ? '0' + String(sec) : String(sec)
         return min.toString() + ":" + secString
       }
 
