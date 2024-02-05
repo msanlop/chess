928c979a6416e17a9f55a46319533766b166971d
Martin <msanlop@github.com>
2024-02-05 18:27:55 +0100

    refactor: make history controls buttons

--- client/src/component/HistoryControls.tsx
+++ client/src/component/HistoryControls.tsx
@@ -1,26 +1,28 @@
-import {ReactComponent as DoubleArrow} from '../res/double-arrow.svg'
-import {ReactComponent as SingleArrow} from '../res/single-arrow.svg'
-import {ReactComponent as SingleBackArrow} from '../res/single-back-arrow.svg'
-import {ReactComponent as DoubleBackArrow} from '../res/double-back-arrow.svg'
-import '../style/HistoryControls.css'
+import { ReactComponent as DoubleArrow } from "../res/double-arrow.svg";
+import { ReactComponent as SingleArrow } from "../res/single-arrow.svg";
+import { ReactComponent as SingleBackArrow } from "../res/single-back-arrow.svg";
+import { ReactComponent as DoubleBackArrow } from "../res/double-back-arrow.svg";
+import "../style/HistoryControls.css";
 
-const HistoryControls = (props:any) => {
-    return(
-        <div className='history-arrows' >
-            <DoubleArrow
-                transform='scale(1.4), rotate(180)'
-                onClick={() => props.onClick('start')}/>
-            <SingleArrow 
-                transform='rotate(180)'
-                onClick={() => props.onClick('step-back')}/>
-            <SingleArrow 
-                onClick={() => props.onClick('step')}/>
-            <DoubleArrow 
-                transform='scale(1.4)'
-                onClick={() => props.onClick('present')}/>
-        </div>
-    )
-}
-
+const HistoryControls = (props: any) => {
+  //TODO: svg not rotating on mobile
+  //TODO: style the buttons better
+  return (
+    <div className="history-arrows">
+      <button onClick={() => props.onClick("start")}>
+        <DoubleArrow transform="scale(1.4), rotate(180)" />
+      </button>
+      <button onClick={() => props.onClick("step-back")}>
+        <SingleArrow transform="rotate(180)" />
+      </button>
+      <button onClick={() => props.onClick("step")}>
+        <SingleArrow />
+      </button>
+      <button onClick={() => props.onClick("present")}>
+        <DoubleArrow transform="scale(1.4)" />
+      </button>
+    </div>
+  );
+};
 
 export default HistoryControls;
